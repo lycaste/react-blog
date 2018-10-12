41acc80a89b52d466ae3ab14154b71f8eb15583f
BOJOY\yaohao <403666963@qq.com>
2018-10-12 10:08:45 +0800

    aa

---
+++ src/components/TopBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const TopBar = () => (
+  <div
+    className='ui huge top attached fluid secondary menu'
+  >
+    <div className='item' />
+    <div className='item'>
+      <h1
+        className='ui green header'
+        style={{ marginTop: '10px' }}
+      >
+        Fullstack Music
+      </h1>
+    </div>
+    <div className='right menu'>
+          
+            Login
+       
+    </div>
+  </div>
+);
+
+export default TopBar;
